7c16e4c204161747480252cc5681cac6f3698c2c
Thiago Andrade <thiagoh@gmail.com>
2016-12-02 10:28:00 -0500

    javascript angularjs codegen

---
+++ bin/javascript-angular-js-wpm.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+scriptpath=$(cd $(dirname $0) ; pwd -P)
+swagger_path=$(cd "$scriptpath/../" ; pwd -P) 
+# echo $swagger_path
+
+skip_tests=-DskipTests
+
+while [[ $# -gt 1 ]]
+do
+key="$1"
+
+case $key in
+    -e|--env)
+    environment="$2"
+    shift # past argument
+    ;;
+    *)
+            # unknown option
+    ;;
+esac
+shift # past argument or value
+done
+
+mvn clean package $skip_tests
+rm -rf $swagger_path/wpm/output 
+java -jar $swagger_path/modules/swagger-codegen-cli/target/swagger-codegen-cli.jar generate -i $swagger_path/wpm/kparamsothy-PHN-1.0.0-swagger.json -l javascript-angular-js -o $swagger_path/wpm/output
